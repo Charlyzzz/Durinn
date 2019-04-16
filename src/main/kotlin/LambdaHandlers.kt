98e31478d063e2f570e792187b6f918ca6120e23
Erwin Debusschere <erwincdl@gmail.com>
2019-04-15 23:54:21 -0300

    Makes authorization handle lens failures

--- src/main/kotlin/LambdaHandlers.kt
+++ src/main/kotlin/LambdaHandlers.kt
@@ -2,6 +2,7 @@ import io.konform.validation.Invalid
 import io.konform.validation.Valid
 import org.http4k.core.*
 import org.http4k.core.Status.Companion.OK
+import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson.asJsonObject
 import org.http4k.format.Jackson.auto
 import org.http4k.format.Jackson.json
@@ -21,7 +22,7 @@ object AuthorizationHandler : AppLoader {
         "erwin" to listOf("54:11:48:88", "04:67:72:b2:8f:48:80")
     )
 
-    override fun invoke(env: Map<String, String>): HttpHandler = {
+    override fun invoke(env: Map<String, String>): HttpHandler = ServerFilters.CatchLensFailure.then {
         val nuevaAutorizacion = nuevaAutorizacionLens(it)
         when (val validationResult = NuevaAutorizacion.validator(nuevaAutorizacion)) {
             is Valid -> Response(OK).with(
