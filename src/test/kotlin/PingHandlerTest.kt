725709f441c4c1c49987b105af223b466bb2e317
Erwin Debusschere <erwincdl@gmail.com>
2019-04-15 23:54:04 -0300

    Adds test helpers

--- src/test/kotlin/PingHandlerTest.kt
+++ src/test/kotlin/PingHandlerTest.kt
@@ -1,19 +1,13 @@
 import assertk.assert
 import assertk.assertions.isEqualTo
-import org.http4k.serverless.BootstrapAppLoader
-import org.http4k.serverless.lambda.ApiGatewayProxyRequest
-import org.http4k.serverless.lambda.LambdaFunction
 import org.junit.Test
 
 class PingHandlerTest {
 
     @Test
     fun `returns ping when called`() {
-        val lambda = LambdaFunction(mapOf(BootstrapAppLoader.HTTP4K_BOOTSTRAP_CLASS to PingHandler::class.java.name))
-        val response = lambda.handle(ApiGatewayProxyRequest().apply {
-            httpMethod = "GET"
-        })
-
+        val lambda = HttpLambda<PingHandler>()
+        val response = lambda()
         assert(response.body).isEqualTo("pong")
         assert(response.statusCode).isEqualTo(200)
     }
