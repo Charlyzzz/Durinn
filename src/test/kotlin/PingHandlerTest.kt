c204f8eea648d31a7a3b379023231e0cd1de1553
Erwin Debusschere <erwincdl@gmail.com>
2019-04-27 15:51:35 -0300

    Adds content type assertion on ping

--- src/test/kotlin/PingHandlerTest.kt
+++ src/test/kotlin/PingHandlerTest.kt
@@ -1,7 +1,9 @@
 import com.natpryce.hamkrest.and
 import com.natpryce.hamkrest.assertion.assertThat
+import org.http4k.core.ContentType.Companion.TEXT_PLAIN
 import org.http4k.core.Status.Companion.OK
 import org.http4k.hamkrest.hasBody
+import org.http4k.hamkrest.hasContentType
 import org.http4k.hamkrest.hasStatus
 import org.junit.Test
 
@@ -10,7 +12,7 @@ class PingHandlerTest {
     @Test
     fun `returns ping when called`() {
         val response = handlePing(Request())
-        assertThat(response, hasStatus(OK) and hasBody("pong"))
+        assertThat(response, hasStatus(OK) and hasBody("pong") and hasContentType(TEXT_PLAIN))
     }
 
     @Test
